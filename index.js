8356c50c778271780528d12df668de5692e06fed
Teemarie288 <tiffanys228@gmail.com>
2026-08-22 14:15:22 -0400

    update2

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('modal__overlay--loading')
+    const success = document.querySelector('modal__overlay--success')
+    
+    emailjs
+     .sendForm(
+        'service_h3f8ev7',
+        'template_fbv3be8',
+        event.target,
+        'hpe6RM54_LcXh0gay'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly @Tiffanys228@gmail.com" 
+        )
+    })
+}
 
+
+function toggleModal() {}
 
 
 
